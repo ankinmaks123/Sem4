c3d869b84cc36eda2f84f06a8fdab523e850c527
MAX <ankin.maks@mail.ru>
2022-05-27 21:19:07 +0300

    Готовое дз

---
+++ дз3/Program.cs
@@ -0,0 +1,15 @@
+﻿int schet(int number)
+{
+    int count = 0;
+    Console.Write($"Сумма цифр равна {number} = ");
+    while (number > 0)
+    {
+        count += number % 10;
+        number /= 10;
+    }
+    return count;    
+}
+
+Console.WriteLine(schet(54));
+Console.WriteLine(schet(44));
+Console.WriteLine(schet(176230));
